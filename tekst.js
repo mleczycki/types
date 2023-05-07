3b78e016edb217742dce8d0d6a8dffdc42150059
mleczycki <leczycki.michal@icloud.com>
2023-05-07 12:03:14 +0200

    Initial commit

---
+++ tekst.js
@@ -0,0 +1,5 @@
+var text = "PISZĄC WIADOMOŚCI, NIE POWINIENEŚ KRZYCZEĆ";
+var presentableText = text.toLowerCase();
+if (presentableText.length > 0) {
+    alert(presentableText);
+}
---
+++ typy2.js
@@ -0,0 +1,6 @@
+if (99 == "99") {
+    console.log("Liczba jest równa łańcuchowi znaków!");
+
+} else {
+    console.log("Przecież liczba nie będzie równa łańcuchowi.");
+}
